e4e4a56998b93143fb04d027d6f34846a37adeab
gsynnestvedt <gsynnestvedt@mail.pima.edu>
2025-02-07 00:32:22 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,9 +1,8 @@
 #Author: Garrison Synnestvedt
 #First Python lab in my CIS129 course
 #Prints a simple short story
-
-print('A cowboy rides into town on a Friday.')
-print('He stays in town for two days.')
-print('Then he leaves on a Friday.')
-print('How can this be?')
+print("A cowboy rides into town on a Friday.")
+print("He stays in town for two days.")
+print("Then he leaves on a Friday.")
+print("How can this be?")
 print("The horse's name is Friday.")
